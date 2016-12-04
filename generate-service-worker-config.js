170f12c44a481118e58f641e01d138a6614f8bf7
MarkTiedemann <www.marktiedemann@gmail.com>
2016-12-04 23:11:07 +0100

    Refactor generate-service-worker-config

--- generate-service-worker-config.js
+++ generate-service-worker-config.js
@@ -5,60 +5,61 @@ const glob = require('glob-all')
 
 // TODO: Publish as a new module on npm
 
-const list = [
-  'bundle.js',
-  'service-worker-config.js',
-  'media/favicons/favicon.ico',
-  'media/favicons/manifest.json',
-  'vendors/font-awesome/css/font-awesome.min.css',
-  'vendors/font-awesome/fonts/fontawesome-webfont.woff2?v=4.7.0'
-]
-
-const patterns = [
-  'media/*'
-]
-
-const generateConfig = ({
+const generateServiceWorkerConfig = ({
   inputDirectory = 'src',
   outputDirectory = 'dist',
-  name = 'service-worker-config.js',
-  global = 'serviceWorkerConfig',
-  root = true,
-  resourceList = [],
-  resourcePatterns = [],
-  relativePath = '',
+  serviceWorkerName = 'service-worker-config.js',
+  serviceWorkerRelativePath = '',
+  navigatorProperty = 'serviceWorkerConfig',
+  prefetchRoot = true,
+  prefetchResources = [],
+  prefetchResourceGlobs = [],
+  globOptions = {},
   verbose = false
 } = {}) => {
 
-  const foundResources = glob.sync(resourcePatterns, { cwd: inputDirectory, nodir: true })
-  const allResources = resourceList.concat(foundResources)
+  const foundResources = glob.sync(prefetchResourceGlobs, 
+    Object.assign(globOptions, { cwd: inputDirectory, nodir: true }))
 
-  if (root) {
+  const allResources = prefetchResources.concat(foundResources)
+
+  if (prefetchRoot) {
     allResources.unshift('')
   }
 
-  const absoluteResources = allResources.map(res => `${relativePath}/${res}`)
+  const resources = allResources.map(res => `${serviceWorkerRelativePath}/${res}`)
 
   if (verbose) {
-    for (let res of absoluteResources) console.log(`Added '${res}' to be prefetched`)
+    for (let res of resources) console.log(`Added '${res}' to be prefetched`)
   }
 
-  const resources = absoluteResources.map(res => `    '${res}'`).join(',\n')
+  const resourcesAsString = resources.map(res => `    '${res}'`).join(',\n')
     
   const template =
-`navigator.${global} = {
+`navigator.${navigatorProperty} = {
   prefetch: [
-${resources}
+${resourcesAsString}
   ]
 }`
 
-  fs.writeFileSync(path.join(outputDirectory, name), template)
+  const filePath = path.join(outputDirectory, serviceWorkerName)
+  fs.writeFileSync(filePath, template)
+  if (verbose) console.log(`Saved Service Worker config in '${filePath}'`)
 }
 
-generateConfig({ 
+generateServiceWorkerConfig({ 
   outputDirectory: process.env.NODE_ENV === 'production' ? 'docs' : 'src',
-  resourceList: list,
-  resourcePatterns: patterns,
-  relativePath: process.env.NODE_ENV === 'production' ? '/angular.hamburg' : '',
+  prefetchResources: [
+    'bundle.js',
+    'service-worker-config.js',
+    'media/favicons/favicon.ico',
+    'media/favicons/manifest.json',
+    'vendors/font-awesome/css/font-awesome.min.css',
+    'vendors/font-awesome/fonts/fontawesome-webfont.woff2?v=4.7.0'
+  ],
+  prefetchResourceGlobs: [
+    'media/*'
+  ],
+  serviceWorkerRelativePath: process.env.NODE_ENV === 'production' ? '/angular.hamburg' : '',
   verbose: true
 })
